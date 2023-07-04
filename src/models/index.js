1c2199561909141bee8fde55fb0a7bbc86e2fe08
Laith-Saleem <67604121+Laith-Vlad@users.noreply.github.com>
2023-07-04 17:18:15 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,17 @@
 'use strict';
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const URI = process.env.DBURI;
-// Configure and initialize Sequelize
-const sequelize = new Sequelize(`${URI}`, {
-  // Additional Sequelize configurations if needed
-});
+const URI = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DB_URL;
+
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+const sequelize = new Sequelize(URI, DATABASE_CONFIG);
 
 // Import models
 
